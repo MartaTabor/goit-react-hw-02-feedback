ddf5de3ab4bdec972cfb4c4f41cecae2ef9d0d98
Marta Tabor <leeloothefifth@googlemail.com>
2024-04-11 21:04:28 +0100

    sections add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react';
+import { FeedbackOptions } from './FeedbackOptions';
+import { Statistics } from './Statistics';
+
+const INITIAL_STATE = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
 export const App = () => {
+  const [feedback, setFeedback] = useState({ ...INITIAL_STATE });
+  const options = Object.keys(INITIAL_STATE);
+
+  const handleFeedback = option => {
+    setFeedback(prevFeedback => ({
+      ...prevFeedback,
+      [option]: prevFeedback[option] + 1,
+    }));
+  };
+
+  const { good, neutral, bad } = feedback;
+  const total = good + bad + neutral;
+  const positivePercentage = total > 0 ? Math.round((good / total) * 100) : 0;
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <FeedbackOptions options={options} onLeaveFeedback={handleFeedback} />
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={total}
+        positivePercentage={positivePercentage}
+      />
     </div>
   );
 };
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+}) => {
+  return (
+    <div>
+      <h2>Statistics</h2>
+      <ul>
+        <li>Good: {good}</li>
+        <li>Neutral: {neutral}</li>
+        <li>Bad: {bad}</li>
+        <li>Total: {total}</li>
+        <li>Positive Feedback: {positivePercentage}</li>
+      </ul>
+    </div>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
